713825cb2a2ccaa68dfcdf45493269bc4076583a
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-06-24 16:34:07 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = env.HOSTNAME || "example.com"; // 使用 env 设置的 HOSTNAME，如果不存在则使用 "example.com"
+      let new_request = new Request(url, request);
+      return fetch(new_request);
     }
-  };
+    // Otherwise, serve the static assets.
+    return env.ASSETS.fetch(request);
+  },
+};
