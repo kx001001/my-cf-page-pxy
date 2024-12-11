0e2992232487cc25b5c8a765c74d068c5a011ff0
MCX <48275131+kx001001@users.noreply.github.com>
2024-12-11 19:11:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,6 +3,7 @@ export default {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
       url.hostname = env.HOSTNAME || "example.com"; // 使用 env 设置的 HOSTNAME，如果不存在则使用 "example.com"
+      url.port = env.PORT || "443";
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
