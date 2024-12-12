e9be4a5275691238a38f6356ac47194e0353f1dc
MCX <48275131+kx001001@users.noreply.github.com>
2024-12-13 00:06:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,11 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
+    let targetProtocol = env.PROTOCOL || "https";  // 新增，从环境变量获取协议，如果没有设置默认用https
+    let targetPort = "443";
+    if (targetProtocol === "http") {
+        targetPort = env.PORT || "80";  // 如果协议是http，端口默认设置为80或者使用环境变量里设置的端口
+    }
     if (url.pathname.startsWith('/')) {
       url.hostname = env.HOSTNAME || "example.com"; // 使用 env 设置的 HOSTNAME，如果不存在则使用 "example.com"
       url.port = env.PORT || "443";
